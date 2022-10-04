45e74872ec6caa887876264f005f752c283b61e0
Jim Phieffer <phiefferj24@columbusacademy.org>
2022-10-04 09:11:50 -0400

    Edited build scripts

--- build.mac.sh
+++ build.mac.sh
@@ -1,6 +1,7 @@
 rm -r ./out/mac/*
 mkdir -p out/mac
 for target in aarch64-apple-darwin x86_64-apple-darwin; do
+    rustup target add $target
     cargo build --release --target $target
     mkdir -p ./out/mac/$target
     cp ./target/$target/release/soshiki-proxy-desktop* ./out/mac/$target/
--- build.win.sh
+++ build.win.sh
@@ -1,10 +1,10 @@
 rm -r ./out/win/*
 mkdir -p out/win
-foreach ($target in "aarch64-pc-windows-msvc","x86_64-pc-windows-gnu","i686-pc-windows-gnu") {
+for target in aarch64-pc-windows-msvc x86_64-pc-windows-gnu i686-pc-windows-gnu; do
     rustup target add $target
     cargo build --release --target $target
     mkdir -p ./out/win/$target
     cp ./target/$target/release/soshiki-proxy-desktop* ./out/win/$target/
     rm ./out/win/$target/soshiki-proxy-desktop.d
-}
+done
 exit 0
